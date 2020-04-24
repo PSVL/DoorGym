1208b1d73d7afdd0e301bacf829de1e9204c00fd
Yusuke urakami~ <yusuke.urakami@us.panasonic.com>
2020-04-24 16:39:28 -0700

    added the control limit in blue xml for position control

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -92,7 +92,7 @@ def get_args():
     parser.add_argument(
         '--eval-interval',
         type=int,
-        default=5,
+        default=20,
         help='eval interval, one eval per n updates (default: None)')
     parser.add_argument(
         '--num-env-steps',
