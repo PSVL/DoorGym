a93662080dfcd730480868f275dcf1dc5d57a64f
kuolunwang <kuolunwang.ee08@nycu.edu.tw>
2022-07-26 17:51:34 +0800

    edit training script

--- train_push.sh
+++ train_push.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python3 train.py --env-name doorenv-v0 --algo ppo --num-steps 4096 --num-processes 8 --lr 1e-3 --save-name husky_ur5_push_3dof --world-path ~/DoorGym/world_generator/husky-ur5-push/pull_husky_ur5/
+python3 train.py --env-name doorenv-v0 --save-name husky_ur5_push_3dof --world-path ~/DoorGym/world_generator/husky-ur5-push/pull_husky_ur5/
